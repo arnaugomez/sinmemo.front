2ee0bd1a4607b3a7959d0e58f9cdca3ad04b7e2b
Arnau Gómez <agomez@mrmilu.com>
2022-09-28 09:32:39 +0200

    feat(service locator): add documentation

--- src/core/auth/AuthModule.ts
+++ src/core/auth/AuthModule.ts
@@ -1,6 +1,8 @@
 import { _authRepository } from "./data/repositories/AuthRepository";
 import { _registerUseCase } from "./domain/use-cases/RegisterUseCase";
 
+/** Service locator for dependency injection of auth related use cases,
+ *  services and repositories */
 export const AuthModule = {
   IAuthRepository: () =>
     import("./data/repositories/AuthRepository").then((v) => v._authRepository),
